809a1d289f4cf661bfae2c360ee4de328d5215f3
Cam A <camdappel@gmail.com>
2022-10-26 12:04:04 +0100

    Commit all

---
+++ Python/0_script_template.py
@@ -0,0 +1,18 @@
+###################
+# Author:
+# Date commenced:
+# Date finalised:
+###################
+#
+# Description: <describe what this file does, use as much space as you need>
+#
+#
+#
+#
+###################
+#
+# Outputs: <if applicable>
+#
+#
+#
+####################
---
+++ R/0_script_template.R
@@ -0,0 +1,39 @@
+###################
+# Author:
+# Date commenced:
+# Date finalised:
+###################
+#
+# Description: <describe what this file does, use as much space as you need>
+#
+#
+#
+#
+###################
+#
+# Outputs: <if applicable>
+#
+#
+#
+####################
+
+# 1. Set all file paths, in, out, data dir, fig dir, etc ---------------------
+
+path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(path)
+path
+
+data_dir<-paste0("")
+
+# 2. Load libraries              -----------------------------
+
+
+# 3. <name here>    -----------------------------
+
+
+# 4. <name here>   -----------------------------
+
+
+# 5. <name here>  -----------------------------
+
+
